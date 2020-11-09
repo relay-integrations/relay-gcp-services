a42d6084dfaeb3ada2e66b9707be1153e4fb1044
Hunter Haugen <hunter@haugens.org>
2020-11-09 14:38:11 -0800

    Add services enable

---
+++ steps/enable/README.md
@@ -0,0 +1,3 @@
+# gcp-services-step-enable
+
+This GCP Services step container enables GCP services for a given project. For more information, check out [GCP documentation here](https://cloud.google.com/endpoints/docs/openapi/enable-api).
---
+++ steps/enable/step.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -euo pipefail
+
+#
+# Commands
+#
+
+GCLOUD="${GCLOUD:-gcloud}"
+JQ="${JQ:-jq}"
+NI="${NI:-ni}"
+
+#
+# Variables
+#
+
+WORKDIR="${WORKDIR:-/workspace}"
+
+log() {
+  echo "[$( date -Iseconds )] $*"
+}
+
+err() {
+  log "error: $*" >&2
+  exit 2
+}
+
+usage() {
+  echo "usage: $*" >&2
+  exit 1
+}
+
+# This reads google.serviceAccountInfo and creates /workspace/creds/credentials.json
+$NI gcp config -d "${WORKDIR}/creds"
+
+[ -f "${WORKDIR}/creds/credentials.json" ] || usage "spec: Unable to find credentials"
+
+declare -a AUTH_ARGS
+
+ACCOUNT="$( $NI get -p '{ .account }' )"
+[ -n "${ACCOUNT}" ] && AUTH_ARGS+=( "${ACCOUNT}" )
+AUTH_ARGS+=( "--key-file=${WORKDIR}/creds/credentials.json" )
+$GCLOUD auth activate-service-account "${AUTH_ARGS[*]}"
+
+declare -a SERVICES
+SERVICES=( $( $NI get | $JQ -r 'try .services // empty | @sh' ) )
+[[ ${#SERVICES[@]} -eq 0 ]] && usage "spec: specify \`services\`"
+
+$GCLOUD services enable "${SERVICES[*]}"
